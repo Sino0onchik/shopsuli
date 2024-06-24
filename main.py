dbbc4d3d5983f523366551e4678f412142836539
Sino0on <dastan.kubanuulu1@gmail.com>
2024-06-25 02:00:17 +0600

    das

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from aiogram.types import Message
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = "5346235377:AAGg1mWc4FPRxGn1GFcnOBcj75MMLlrAJlA"
 OPENAI_API_KEY = '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'
-DJANGO_API_URL = 'http://127.0.0.1:8033/product-list-api/'
+DJANGO_API_URL = 'https://melsov.ru/product-list-api/'
 
 # All handlers should be attached to the Router (or Dispatcher)
 
